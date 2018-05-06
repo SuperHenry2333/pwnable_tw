5d77f5e8e0a2ed7a7c2fdb8859dc2c8d3d2fefdc
SuperHenry2333 <931851825@qq.com>
2018-05-06 09:57:57 +0800

    ubuntu16

---
+++ calc/test.c
@@ -0,0 +1,6 @@
+#include "unistd.h"
+int main(int argc, char const *argv[])
+{
+	void *a=malloc(1);
+	return 0;
+}
--- commit.sh
+++ commit.sh
@@ -3,4 +3,4 @@ git add --all
 git commit -m $1
 # git remote add origin git@github.com:michaelliao/learngit.git
 # git remote add origin https://github.com/SuperHenry2333/pwnable_tw.git
-git push -u origin master
+git push -u git@github.com:SuperHenry2333/pwnable_tw.git master
