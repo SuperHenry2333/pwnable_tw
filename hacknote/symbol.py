63d81eaba99cf936756401a88f479f53c65b85f3
SuperHenry2333 <931851825@qq.com>
2018-05-20 17:09:02 +0800

    clean

--- hacknote/symbol.py
+++
@@ -1,8 +0,0 @@
-from pwn import *
-e=ELF('/lib/i386-linux-gnu/libc-2.23.so')
-# print hex(e.address)
-# print hex(e.symbols['malloc'])
-print hex(e.symbols['system'])
-# print hex(e.symbols['puts'])
-# print hex(e.symbols['system']-e.symbols['fread'])
-# print hex(0x45390-0x6e1a0)
